c29af404960af986fd492705c8a2d0fa3e4acdca
Giuseppe Grieco <g.grieco@dacolabs.com>
2026-01-29 13:36:16 +0100

    fixup! feat: python schema translator

--- cmd/daco/internal/app.go
+++ cmd/daco/internal/app.go
@@ -10,11 +10,13 @@ import (
 	"github.com/dacolabs/cli/internal/commands"
 	"github.com/dacolabs/cli/internal/translate"
 	"github.com/dacolabs/cli/internal/translate/pyspark"
+	"github.com/dacolabs/cli/internal/translate/python"
 )
 
 func registerTranslators() translate.Register {
 	translators := make(translate.Register)
 	translators["pyspark"] = &pyspark.Translator{}
+	translators["python"] = &python.Translator{}
 	return translators
 }
 
