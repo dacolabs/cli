a1ed0139b60c25f80fc92a92a56ff95a2470c206
Giuseppe Grieco <g.grieco@dacolabs.com>
2026-01-29 13:14:53 +0100

    feat: add avro schema translator

--- cmd/daco/internal/app.go
+++ cmd/daco/internal/app.go
@@ -9,12 +9,14 @@ import (
 
 	"github.com/dacolabs/cli/internal/commands"
 	"github.com/dacolabs/cli/internal/translate"
+	"github.com/dacolabs/cli/internal/translate/avro"
 	"github.com/dacolabs/cli/internal/translate/pyspark"
 )
 
 func registerTranslators() translate.Register {
 	translators := make(translate.Register)
 	translators["pyspark"] = &pyspark.Translator{}
+	translators["avro"] = &avro.Translator{}
 	return translators
 }
 
