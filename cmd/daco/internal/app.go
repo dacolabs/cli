4cb69542c94e2e682232b1cac3800e668b458727
Giuseppe Grieco <g.grieco@dacolabs.com>
2026-01-29 13:37:33 +0100

    feat: scala schema translator

--- cmd/daco/internal/app.go
+++ cmd/daco/internal/app.go
@@ -10,11 +10,13 @@ import (
 	"github.com/dacolabs/cli/internal/commands"
 	"github.com/dacolabs/cli/internal/translate"
 	"github.com/dacolabs/cli/internal/translate/pyspark"
+	"github.com/dacolabs/cli/internal/translate/scala"
 )
 
 func registerTranslators() translate.Register {
 	translators := make(translate.Register)
 	translators["pyspark"] = &pyspark.Translator{}
+	translators["scala"] = &scala.Translator{}
 	return translators
 }
 
