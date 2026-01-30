603beb50454ac331278d6fb7252d3ba7e432c39e
Giuseppe Grieco <g.grieco@dacolabs.com>
2026-01-30 15:59:24 +0100

    feat: python schema translator (#65)
    * feat: python schema translator
    
    * fixup! feat: python schema translator
    
    ---------
    
    Signed-off-by: Gudmundur Orri Palsson <g.palsson@daco.sh>
    Signed-off-by: Giuseppe Grieco <g.grieco@dacolabs.com>
    Co-authored-by: Gudmundur Orri Palsson <g.palsson@daco.sh>

--- cmd/daco/internal/app.go
+++ cmd/daco/internal/app.go
@@ -11,12 +11,14 @@ import (
 	"github.com/dacolabs/cli/internal/translate"
 	"github.com/dacolabs/cli/internal/translate/databrickssql"
 	"github.com/dacolabs/cli/internal/translate/pyspark"
+	"github.com/dacolabs/cli/internal/translate/python"
 	"github.com/dacolabs/cli/internal/translate/scala"
 )
 
 func registerTranslators() translate.Register {
 	translators := make(translate.Register)
 	translators["pyspark"] = &pyspark.Translator{}
+	translators["python"] = &python.Translator{}
 	translators["databricks-sql"] = &databrickssql.Translator{}
 	translators["scala"] = &scala.Translator{}
 	translators["spark-scala"] = &pyspark.Translator{}
