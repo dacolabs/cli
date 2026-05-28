a55b117b087d30c7a9bbcea41cbe493264d4baf8
gummiorri <g.palsson@dacolabs.com>
2026-05-28 20:59:43 +0200

    feat: add favicon and change quote

--- landing/components/Quote.tsx
+++ landing/components/Quote.tsx
@@ -4,7 +4,7 @@ export default function Quote() {
       <div className="container">
         <blockquote className="quote">
           <span className="quotemark">&ldquo;</span>
-          We were tired of having to leave our local development to test our pipelines.
+          We were tired of having to leave our local development environment to test our pipelines.
           <span className="quotemark">&rdquo;</span>
         </blockquote>
         <div className="quote-attr">
